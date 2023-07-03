0aac87cff8aacfec37813efe3f2c195d30ffa25d
VINANK <neupaneayush3@gmail.com>
2023-07-03 17:22:29 +0545

    Html templatin

--- project/Django_Blog/djangonautic/djangonautic/views.py
+++ project/Django_Blog/djangonautic/djangonautic/views.py
@@ -1,7 +1,10 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
-def home(Response):
-	return HttpResponse("Home Page")
+def home(request):
+	# return HttpResponse("Home Page")
+	return render(request,"homepage.html")
 
-def about(Response):
-	return HttpResponse("about page")
+def about(request):
+	# return HttpResponse("about page")
+	return render(request,"aboutpage.html")
