c171d1303d743e861605cb9558592199a648780e
VINANK <neupaneayush3@gmail.com>
2023-07-06 13:20:40 +0545

    uploading media, accounts and user creation forms

--- project/Django_Blog/djangonautic/djangonautic/urls.py
+++ project/Django_Blog/djangonautic/djangonautic/urls.py
@@ -2,9 +2,12 @@ from django.contrib import admin
 from django.urls import path,include
 from . import views		# importing views.py from the same directory
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('accounts/',include('accounts.urls')),
     path('articles/',include('articles.urls')),	#registering the urls of the articles app
     path('about/',views.about),		#url route which sends about function from views.py
     path('',views.home)				#url route which sends home function from views.py
@@ -12,3 +15,4 @@ urlpatterns = [
 
 
 urlpatterns+=staticfiles_urlpatterns()
+urlpatterns+=static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
