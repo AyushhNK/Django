6f42d2fa9e006354945a66caa96975957ada35bd
VINANK <neupaneayush3@gmail.com>
2023-07-07 15:40:24 +0545

    model forms,foreign keys, checking login status and redirecting to the hompage

--- project/Django_Blog/djangonautic/djangonautic/urls.py
+++ project/Django_Blog/djangonautic/djangonautic/urls.py
@@ -4,13 +4,14 @@ from . import views		# importing views.py from the same directory
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from django.conf.urls.static import static
 from django.conf import settings
+from articles import views as article_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/',include('accounts.urls')),
     path('articles/',include('articles.urls')),	#registering the urls of the articles app
     path('about/',views.about),		#url route which sends about function from views.py
-    path('',views.home)				#url route which sends home function from views.py
+    path('',article_views.article_list,name="HOME")				#url route which sends home function from views.py
 ]
 
 
