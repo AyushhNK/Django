547d50ee74725622ba880b86438f5a4c68c0b296
VINANK <neupaneayush3@gmail.com>
2024-01-09 11:06:01 +0545

    login completed

--- learning/authentication_and_authorization/login_signup/views.py
+++ learning/authentication_and_authorization/login_signup/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from .forms import SignUpForm
 from django.http import HttpResponse
 from django.contrib.auth.models import User
+from django.contrib.auth import login,authenticate
+from django.contrib.auth.forms import AuthenticationForm
 
 # Create your views here.
 def signup(request):
@@ -16,3 +18,19 @@ def signup(request):
 	else:	
 		form=SignUpForm()
 	return render(request,"signup.html",{"form":form})
+
+def logins(request):
+	if request.method=="POST":
+		form=AuthenticationForm(request,data=request.POST)
+		
+		if form.is_valid():
+			username=form.cleaned_data["username"]
+			password=form.cleaned_data["password"]
+			user=authenticate(username=username,password=password)
+			if user is not None:
+				login(request,user)
+				name=request.user.username
+				return render(request,"login.html",{"user":name})
+	else:	
+		form=AuthenticationForm()
+	return render(request,"login.html",{"form":form})
