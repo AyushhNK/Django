98a56511ee148276211a36b2cf9ca3a83bc18d3a
VINANK <neupaneayush3@gmail.com>
2024-01-10 11:43:11 +0545

    added logout

--- project/Ecommerce_site/Ecommerce/urls.py
+++ project/Ecommerce_site/Ecommerce/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns=[
 	path("",views.home,name="home"),
 	path("login/",views.log_in,name="log_in"),
-	path("signup/",views.signup,name="signup")
+	path("signup/",views.signup,name="signup"),
+	path("logout/",views.log_out,name="log_out")
 ]
--- project/Ecommerce_site/Ecommerce/views.py
+++ project/Ecommerce_site/Ecommerce/views.py
@@ -1,15 +1,43 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 from django.http import HttpResponse
+from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
+from django.contrib.auth import login,authenticate,logout
+from .forms import SignUpForm
 
 # Create your views here.
 def home(request):
-	return render(request,"home.html")
+	
+	user=request.user
+	return render(request,"home.html",{"name":user})
 
 
 
 def log_in(request):
-	return render(request,"login.html")
+	if request.method=="POST":
+		form=AuthenticationForm(request,data=request.POST)
+		if form.is_valid():
+			username=form.cleaned_data["username"]
+			password=form.cleaned_data["password"]
+			user=authenticate(username=username,password=password)
+			if user is not None:
+				login(request,user)
+				return redirect("home")
+	else:
+		form=AuthenticationForm()
+	return render(request,"login.html",{"form":form})
 
 
 def signup(request):
-	return render(request,"signup.html")
+	if request.method=="POST":
+		form=SignUpForm(request.POST)
+		if form.is_valid():
+			user=form.save()
+			login(request,user)
+			return redirect("home")
+	else:
+		form=SignUpForm()
+	return render(request,"signup.html",{"form":form})
+
+def log_out(request):
+	logout(request)
+	return redirect('home')
