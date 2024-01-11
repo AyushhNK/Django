2127c0834a3d5e341634a42a6f0ca54dbc943226
VINANK <neupaneayush3@gmail.com>
2024-01-11 17:44:37 +0545

    added add to cart remove form cart

--- project/Ecommerce_site/Ecommerce/urls.py
+++ project/Ecommerce_site/Ecommerce/urls.py
@@ -5,5 +5,8 @@ urlpatterns=[
 	path("",views.home,name="home"),
 	path("login/",views.log_in,name="log_in"),
 	path("signup/",views.signup,name="signup"),
-	path("logout/",views.log_out,name="log_out")
+	path("logout/",views.log_out,name="log_out"),
+	path('add-to-cart/<int:product_id>/',views.add_to_cart, name='add_to_cart'),
+	path('usercart/',views.items_in_cart,name="items_cart"),
+	path('removecart/<int:cart_item_id>',views.remove_from_cart,name="remove_cart"),
 ]
--- project/Ecommerce_site/Ecommerce/views.py
+++ project/Ecommerce_site/Ecommerce/views.py
@@ -3,14 +3,16 @@ from django.http import HttpResponse
 from django.contrib.auth.forms import AuthenticationForm,UserCreationForm
 from django.contrib.auth import login,authenticate,logout
 from .forms import SignUpForm
+from .models import Product,Cart,CartItem
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
+
 def home(request):
-	
 	user=request.user
-	return render(request,"home.html",{"name":user})
-
-
+	obj=Product.objects.all()
+	return render(request,"home.html",{"name":user,"objects":obj})
 
 def log_in(request):
 	if request.method=="POST":
@@ -41,3 +43,37 @@ def signup(request):
 def log_out(request):
 	logout(request)
 	return redirect('home')
+
+
+@login_required
+def add_to_cart(request, product_id):
+    product = get_object_or_404(Product, id=product_id)
+    user_cart, created = Cart.objects.get_or_create(user=request.user)
+    cart_item, created = CartItem.objects.get_or_create(cart=user_cart, product=product)
+    if not created:
+        cart_item.quantity += 1
+        cart_item.save()
+    return redirect('home')
+
+@login_required
+def items_in_cart(request):
+	user_cart= Cart.objects.get(user=request.user)
+	cart_items= CartItem.objects.filter(cart=user_cart)
+	total=0
+	for i in cart_items:
+		total=i.quantity*i.product.price
+	return render(request,"cart.html",{"items":cart_items,"totaldue":total})
+
+
+
+@login_required
+def remove_from_cart(request, cart_item_id):
+	cart_item = CartItem.objects.filter(id=cart_item_id)
+	for item in cart_item:
+		if item.quantity>1:
+			item.quantity-=1
+			item.save()
+		else:
+			item.delete()
+	return redirect("items_cart")
+	
