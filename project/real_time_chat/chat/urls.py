1a78c833153d53490c0fbf2a1eccdc115f87b16d
VINANK <neupaneayush3@gmail.com>
2024-01-19 14:04:34 +0545

    added private message

--- project/real_time_chat/chat/urls.py
+++ project/real_time_chat/chat/urls.py
@@ -5,8 +5,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
 	path("", chat_views.chatPage, name="chat-page"),
-
-	# login-section
 	path("auth/login/", LoginView.as_view(template_name="LoginPage.html"), name="login-user"),
 	path("auth/logout/", LogoutView.as_view(), name="logout-user"),
+    path('send_message/<str:recipient_username>/', chat_views.send_message, name='send_message'),
 ]
--- project/real_time_chat/chat/views.py
+++ project/real_time_chat/chat/views.py
@@ -1,8 +1,41 @@
+# views.py
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.db.models import Q
+from .models import Message
+from .forms import MessageForm
+
+def inbox(request):
+    received_messages = Message.objects.filter(receiver=request.user).order_by('-timestamp')
+    return render(request, 'inbox.html', {'received_messages': received_messages})
+
+def send_message(request, recipient_username):
+    recipient = User.objects.get(username=recipient_username)
+    recipient_id=recipient.id
+    sender=User.objects.get(username=request.user)
+    sender_id=sender.id
+
+    if request.method == 'POST':
+        form = MessageForm(request.POST)
+        if form.is_valid():
+            message = form.save(commit=False)
+            message.sender = request.user
+            message.receiver = recipient
+            message.save()
+            messages.success(request, 'Message sent successfully!')
+            return redirect(f'/send_message/{recipient_username}')
+    else:
+        form = MessageForm()
+    received_messages =Message.objects.filter(
+        Q(sender=request.user, receiver_id=recipient_id) | Q(sender_id=recipient_id, receiver=request.user)
+    ).order_by('timestamp')
+    return render(request, 'inbox.html', {'form': form, 'recipient': recipient,'received_messages': received_messages})
+
 
 
 def chatPage(request, *args, **kwargs):
 	if not request.user.is_authenticated:
 		return redirect("login-user")
-	context = {}
-	return render(request, "room.html", context)
+	users=User.objects.exclude(username=request.user).all()
+	return render(request, "room.html",{"users":users})
