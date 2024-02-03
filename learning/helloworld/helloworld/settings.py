77e8aa84a43c3887e2fd37fb3cf246f81eeea63e
VINANK <neupaneayush3@gmail.com>
2024-02-03 16:59:35 +0545

    added postgres connection

--- learning/helloworld/helloworld/settings.py
+++ learning/helloworld/helloworld/settings.py
@@ -76,11 +76,14 @@ WSGI_APPLICATION = 'helloworld.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django_db',
+        'USER': 'postgres',
+        'PASSWORD': 'test123',
+        'HOST': 'localhost',  # Replace with your PostgreSQL server's address if necessary
+        'PORT': '5432',          # Leave empty to use the default PostgreSQL port (usually 5432)
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
