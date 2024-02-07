e999f88c86386ce6a71cde2a508abc8d0213dd73
VINANK <neupaneayush3@gmail.com>
2024-02-07 14:41:46 +0545

    jwt token

--- learning/jwtauthentication/authentication/views.py
+++ learning/jwtauthentication/authentication/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# yourapp/views.py
 
-# Create your views here.
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
+
+@api_view(['GET'])
+@permission_classes([IsAuthenticated])
+def index(request):
+    return Response({'message': 'This is a protected view!'})
