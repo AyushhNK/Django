78f464b598fd8d5195967fd9722f5e3a6406662f
AyushhNK <neupaneayush3@gmail.com>
2026-01-08 17:56:45 +0545

    feat(chatapp): django channels

--- project/real_time_chat/real_time_chat/asgi.py
+++ project/real_time_chat/real_time_chat/asgi.py
@@ -1,7 +1,7 @@
 import os
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ChatApp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'real_time_chat.settings')
 
 from channels.auth import AuthMiddlewareStack
 from channels.routing import ProtocolTypeRouter , URLRouter
